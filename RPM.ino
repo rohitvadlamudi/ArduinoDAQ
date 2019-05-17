b4e7e89bdd7bb015235d97d005e0ef05b05ad5e0
rohitvadlamudi <50248818+rohitvadlamudi@users.noreply.github.com>
2019-05-17 03:43:45 +0100

    Add files via upload

---
+++ RPM.ino
@@ -0,0 +1,23 @@
+#include <FreqMeasure.h>
+
+void setup() {
+  Serial.begin(57600);
+  FreqMeasure.begin();
+}
+
+double sum=0;
+int count=0;
+
+void loop() {
+  if (FreqMeasure.available()) {
+    // average several reading together
+    sum = sum + FreqMeasure.read();
+    count = count + 1;
+    if (count > 30) {
+      float frequency = FreqMeasure.countToFrequency(sum / count);
+      Serial.println(frequency);
+      sum = 0;
+      count = 0;
+    }
+  }
+}
